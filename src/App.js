4086cb648ae5852c56538d849ab21d3e958958eb
aidraliev <aa11941@auca.kg>
2025-12-02 09:23:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg'
 import './App.css'
-import { useState } from 'react'
+import { useDebugValue, useEffect, useState } from 'react'
 import Header from './components/header/header'
 import Content from './components/content/content'
 import Footer from './components/footer/footer'
+import Counter from './counter'
+import ColorPicker from './color_picker/ColorPicker'
 
 function App() {
+	const [count, setCount] = useState(0)
+
+	useEffect(() => {
+		document.title = `Count:${count}`
+	}, [count])
+
 	return (
 		<div className='App'>
-			<Header />
-			<Content />
-			<Footer />
+			<ColorPicker />
 		</div>
 	)
 }
