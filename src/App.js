74373a8a33250e8eafde30f43a9ac25eded4197f
aidraliev <aa11941@auca.kg>
2025-12-13 14:05:19 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg'
 import './App.css'
 import { useEffect, useState } from 'react'
-// Assuming Header, Content, Footer, Counter, and ColorPicker are not strictly needed for this specific guest list functionality
 // import Header from './components/header/header'
 // import Content from './components/content/content'
 // import Footer from './components/footer/footer'
